300bf17196394e4d30393cc977ff40bc33329b1e
Shailendra Pal <shailendrapal100@gmail.com>
2017-05-21 16:59:28 +1000

    Save new revisions when they're available

--- app/controllers/articles.js
+++ app/controllers/articles.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose'),
   _ = require('lodash'),
+  moment = require('moment'),
   request = require('request'),
   async = require('async');
 
@@ -7,7 +8,7 @@ var Article = mongoose.model('Article');
 var User = mongoose.model('User');
 module.exports = {
   render: function (req, res, next) {
-    var mediaWikiAPI = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=user|comment|timestamp|sha1|parsedcomment&format=json&titles=';
+    var mediaWikiAPI = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=flags|ids|size|user|timestamp|sha1|parsedcomment&format=json&titles=';
     var queryParam = req.query.search;
 
     if (!queryParam || !queryParam.split('-')[0]) {
@@ -18,16 +19,75 @@ module.exports = {
       });
     } else {
       var articleTitle = queryParam.split('-')[0].trim();
+      var mediaWikiRevisions = [];
 
-      async.parallel([
+      async.series([
         function (cb) {
-          Article.findOne({
-            title: articleTitle
-          }, {}, {
-            sort: {
-              timestamp: -1
+          async.waterfall([
+            function (callback) {
+              Article.findOne({
+                title: articleTitle
+              }, {}, {
+                sort: {
+                  timestamp: -1
+                }
+              }, callback);
+            },
+            function (article, callback) {
+              var timePassedSinceLastUpdate = moment().diff(moment(article.timestamp), 'days');
+              if (timePassedSinceLastUpdate <= 1) {
+                callback(null, article);
+              } else {
+                request({
+                  url: mediaWikiAPI + articleTitle,
+                  headers: {
+                    'User-Agent': 'request'
+                  }
+                }, function (err, response, body) {
+                  if (err) {
+                    callback(err);
+                  } else {
+                    var mediaWikiResults = JSON.parse(body);
+                    var mediaWikiResponseValues = _.values(mediaWikiResults.query.pages);
+
+                    if (mediaWikiResponseValues.length > 0) {
+                      mediaWikiRevisions = mediaWikiResponseValues[0].revisions;
+                      // Save only if the revisions are available
+                      if (mediaWikiRevisions && mediaWikiRevisions.length > 0) {
+                        async.each(mediaWikiRevisions, function (revision) {
+                          revision.title = articleTitle;
+                        });
+
+                        mediaWikiRevisions = _.filter(mediaWikiRevisions, function (revision) {
+                          return moment(revision.timestamp).isAfter(moment(article.timestamp));
+                        });
+
+                        // Save mediWikiRevisions to db
+                        Article.insertMany(mediaWikiRevisions, function (err, data) {
+                          if (err) {
+                            callback(err);
+                          } else {
+                            callback(null, article);
+                          }
+                        });
+                      } else {
+                        callback(null, article);
+                      }
+                    } else {
+                      callback(null, article);
+                    }
+                  }
+
+                });
+              }
             }
-          }, cb);
+          ], function (err, article) {
+            if (err) {
+              cb(err);
+            } else {
+              cb(null, article);
+            }
+          });
         },
         function (cb) {
           Article.aggregate([{
@@ -81,23 +141,11 @@ module.exports = {
               ], cb);
             }
           });
-        },
-        function (cb) {
-          request({
-            url: mediaWikiAPI + articleTitle,
-            headers: {
-              'User-Agent': 'request'
-            }
-          }, function (err, response, body) {
-            cb(err, body);
-          });
         }
       ], function (err, results) {
         if (err) {
           next(err);
         } else {
-          var mediaWikiResults = JSON.parse(results[3]);
-
           var article = results[0],
             topEditors = results[2];
 
@@ -109,7 +157,8 @@ module.exports = {
               found: !!article,
               article: article,
               revisions: results[1][0].count,
-              topEditors: topEditors
+              topEditors: topEditors,
+              newRevisions: mediaWikiRevisions && mediaWikiRevisions.length ? mediaWikiRevisions.length : 0
             }
           });
         }
