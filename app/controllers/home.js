d30e479b6dbb7337f523954bd8e0a432bcfd5f44
Shailendra Pal <shailendrapal100@gmail.com>
2017-05-21 14:58:20 +1000

    Complete statistics part

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose'),
+  _ = require('lodash'),
   async = require('async');
 
 var Article = mongoose.model('Article');
@@ -7,20 +8,96 @@ var User = mongoose.model('User');
 module.exports = function (req, res, next) {
   async.parallel([
       aggregateRevisions,
-      aggregateRevisedArticlesByUniqueUsers
+      aggregateRevisedArticlesByUniqueUsers,
+      getArticleRevisionAge
     ],
     function (err, results) {
       if (err) {
         next(err);
       }
-
       res.render('home.pug', {
         data: {
           revisions: results[0],
-          uniqueRevisions: results[1]
+          uniqueRevisions: results[1],
+          history: results[2]
         }
       });
     });
+};
+
+var getArticleRevisionAge = function (cb) {
+  async.waterfall([
+    function (next) {
+      // Get all article names
+      Article.distinct('title', next);
+    },
+    function (articles, next) {
+      async.map(articles, function (articleTitle, callback) {
+        Article.aggregate([{
+            $match: {
+              title: articleTitle
+            }
+          },
+          {
+            $group: {
+              _id: "$title",
+              revisions: {
+                $push: "$timestamp"
+              }
+            }
+          },
+          {
+            $unwind: "$revisions"
+          },
+          {
+            $sort: {
+              revisions: -1
+            }
+          },
+          {
+            $group: {
+              _id: "$_id",
+              createdAt: {
+                $last: "$revisions"
+              },
+              lastModifiedDate: {
+                $first: "$revisions"
+              }
+            }
+          },
+          {
+            $project: {
+              createdAt: "$createdAt",
+              lastModifiedDate: "$lastModifiedDate",
+              age: {
+                $subtract: [new Date(), "$createdAt"]
+              }
+            }
+          }
+        ], function (err, results) {
+          if (err) {
+            callback(err);
+          } else {
+            callback(null, results[0]);
+          }
+        });
+      }, next);
+    }
+  ], function (err, articles) {
+    if (err) {
+      cb(err);
+    } else {
+      // Now we have all the articles and their ages
+      var articlesSortedByAge = _.sortBy(articles, function (article) {
+        return article.age;
+      });
+
+      cb(null, {
+        longest: articlesSortedByAge[articlesSortedByAge.length - 1],
+        shortest: articlesSortedByAge[0]
+      });
+    }
+  });
 };
 
 var aggregateRevisions = function (cb) {
